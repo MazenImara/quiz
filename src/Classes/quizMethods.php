fc6e39df9889208e0b3c42db19dda1b3dbbfbb81
Mazen Imara <mazen.imara@gmail.com>
2017-07-28 14:50:52 +0200

    add exiption

--- src/Classes/quizMethods.php
+++ src/Classes/quizMethods.php
@@ -5,21 +5,20 @@ namespace Drupal\quiz\Classes;
 class quizMethods {
 
 	public static function addQuiz($quiz) {
-		foreach ($quiz as $key => $value) {
-			drupal_set_message($key.': '.$value);
+		try {
+			\Drupal::database()->insert('quiz')
+			                   ->fields([
+					'title',
+					'body'
+				])
+			->values(array(
+					$quiz['title'],
+					$quiz['body'],
+				))
+			->execute();
+		} catch (\Exception $e) {
+			drupal_set_message('Error happen when adding quiz');
 		}
-		drupal_set_message($quiz['title']);
-
-		\Drupal::database()->insert('quiz')
-		                   ->fields([
-				'title',
-				'body'
-			])
-		->values(array(
-				$quiz['title'],
-				$quiz['body'],
-			))
-		->execute();
 	}
 
 }
